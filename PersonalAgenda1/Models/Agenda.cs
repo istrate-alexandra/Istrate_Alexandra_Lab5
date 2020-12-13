4da33eb11ba75b5a70d6719d6ccd5ec7eba352c1
IONUȚ-CRISTIAN CIOCOIU <ionut.ciocoiu@stud.ubbcluj.ro>
2020-12-13 18:24:01 +0200

    Add project files.

---
+++ PersonalAgenda1/Models/Agenda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalAgenda1.Models
+{
+    class Agenda
+    {
+    }
+}
+    public class Agenda
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+
---
+++ PersonalAgenda1/NotePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PersonalAgenda1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotePage : ContentPage
+    {
+        public NotePage()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
